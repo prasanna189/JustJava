db2fc9db581a156497867852289f6837d89beff4
Prasanna Kumar <spkforwork@gmail.com>
2017-03-17 22:28:11 +0530

    another test commit . ignore. last commit for today.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -17,6 +17,8 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
 
+    int quantity=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,9 +29,17 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity=5;
-        display(quantity);
         displayPrice(quantity*5);
+    }
+
+    public void increment(View view){
+        quantity=quantity+1;
+        display(quantity);
+    }
+
+    public void decrement(View view){
+        quantity=quantity-1;
+        display(quantity);
     }
 
     /**
